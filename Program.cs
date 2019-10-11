9c463405519f48371d8ed61e3f15c5b21a65d2a8
Ross Larson <zo0o0ot@gmail.com>
2019-10-11 03:25:01 +0000

    adds .NET Core 3 Console app and .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace historical_nfl_draft_data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This will eventually be useful.");
+        }
+    }
+}
